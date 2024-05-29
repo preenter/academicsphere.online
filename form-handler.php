8e0456ea8ee0eface70a20a595af88a5847f478d
preenter <academicsphereinfo@gmail.com>
2024-05-29 14:38:20 +0500

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -1,30 +1,37 @@
 <?php
+// Retrieve form data
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
+// Validate email
+if (!filter_var($visitor_email, FILTER_VALIDATE_EMAIL)) {
+    echo "Invalid email format";
+    exit;
+}
 
-
+// Define email sender and receiver
 $email_from = 'academicsphereinfo@gmail.com';
-
-
-$email_subject = 'New Form Submission';
-
-$email_body = "User Name: $name\n".
-              "User Name: $visitor_email\n".
-              "User Name: $subject\n".
-              "User Name: $message\n";
-
-
 $to = "mahmad16feb@gmail.com";
 
+// Create email subject and body
+$email_subject = 'New Form Submission';
+$email_body = "User Name: $name\n".
+              "User Email: $visitor_email\n".
+              "Subject: $subject\n".
+              "Message: $message\n";
+
+// Set headers
 $headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-$headers .= "Reply-To: $visitor_email \r\n"
-
-mail($to,$email_subject,$email_body,$headers)
-
-header("Location: contact.html")
-
+// Send email
+if (mail($to, $email_subject, $email_body, $headers)) {
+    // Redirect to contact page
+    header("Location: contact.html");
+    exit;
+} else {
+    echo "Email sending failed.";
+}
 ?>
